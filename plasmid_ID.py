b0664442e7945d5a6108c30fb340bbbee62c89de
ndusek <nicholas.dusek@ndsu.edu>
2024-11-06 14:24:12 -0600

    Removed intermediate distances_merge.txt file (closes #2)

--- plasmid_ID.py
+++ plasmid_ID.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 from Bio import SeqIO
 from Bio.Seq import Seq
@@ -48,26 +44,25 @@ def find_hash_position(sequence, tags):
             return (key, position)
     return ("none", -1)
 
-sample_name = args.seqfile.split('.')[0]
-distances_f = sample_name + "_distances_merge.txt"
-with gzip.open(args.seqfile, "rt") as r1,open(distances_f,"w") as f3:
-    f3.write("fw_name,fw_pos,rev_name,rev_pos,pl_name,pl_pos\n")
+rows = []
+with gzip.open(args.seqfile, "rt") as r1:
     for fw in SeqIO.parse(r1, "fastq") :
-        #print(fw)
         str_seq = str(fw.seq)
         fw_name, fw_pos = find_hash_position(str_seq, forward_dict)
         rv_name, rv_pos = find_hash_position(str_seq, reverse_dict)
         pl_name, pl_pos = find_hash_position(str_seq, plasmids)
-        f3.write(",".join([fw_name, str(fw_pos), rv_name, str(rv_pos), pl_name,str(pl_pos)])) 
-        f3.write("\n")
+        tmp = (fw_name, int(fw_pos), rv_name, int(rv_pos), pl_name, int(pl_pos))
+        rows.append(tmp)
     
-df = pd.read_csv(distances_f)
-df
+colnames=['fw_name','fw_pos','rev_name','rev_pos','pl_name','pl_pos']
+df = pd.DataFrame(rows, columns=colnames)
+df.to_csv('distances_merge.csv', sep=',', index=False)
 
 summ=df[['fw_name', 'rev_name', 'pl_name', 'pl_pos']].groupby(['fw_name', 'rev_name', 'pl_name'], as_index=False).agg( ['count','mean'])
 
 summ.columns = ['fw_name','rev_name','pl_name','count','mean']
 
+sample_name = args.seqfile.split('.')[0]
 outfile = sample_name + "_results.csv"
 summ.to_csv(outfile, sep=',', index=False)
 
